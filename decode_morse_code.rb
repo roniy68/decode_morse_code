7e9b01c06b054613ffe9832dba0d399317dbfddf
ram1117 <ramkumark1@outlook.com>
2023-04-05 19:53:51 +0530

    setup linters and fix linter errors

--- decode_morse_code.rb
+++ decode_morse_code.rb
@@ -8,30 +8,29 @@ MORSE_HASH =
     '-.....' => '6',
     '--...' => '7', '---..' => '8', '----.' => '9', '-----' => '0' }.freeze
 
-# Create a method to decode a Morse code character, takes a string parameter, and return the corresponding character in uppercase (e.g. decode_char(".-") returns "A").
-
+# Create a method to decode a Morse code character, takes a string parameter,
+# and return the corresponding character in uppercase (e.g. decode_char(".-") returns "A").
 
 def decode_morse(message)
-    words = message.split(/   /)
-    arr = Array.new
-    words.each{|word|
-      arr.push decode_word(word)
-    }
-    return arr.join(" ")
+  words = message.split(/   /)
+  arr = []
+  words.each do |word|
+    arr.push decode_word(word)
+  end
+  arr.join(' ')
 end
 
 def decode_word(word)
-    chars= word.split(/ /)
-    str2 =""
-    chars.each{|character| 
-    str2+=decode_char(character)
-    }
-    return str2
+  chars = word.split(/ /)
+  str2 = ''
+  chars.each do |character|
+    str2 += decode_char(character)
+  end
+  str2
 end
 
-def decode_char(stringChar)
-    return MORSE_HASH[stringChar]
+def decode_char(string_char)
+  MORSE_HASH[string_char]
 end
 
 puts decode_morse('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
-
