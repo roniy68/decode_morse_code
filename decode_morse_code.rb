e7f0e198f58b1284b00dbd0b0b264371413e78d9
John Adibe <johnadibe123@gmail.com>
2023-04-05 14:53:33 -0800

    Add decode_char to return A

---
+++ decode_morse_code.rb
@@ -0,0 +1,8 @@
+# Create a method to decode a Morse code character, takes a string parameter, and return the corresponding character in uppercase (e.g. decode_char(".-") returns "A").
+
+def decode_char(stringChar)
+    if(stringChar === ".-")
+       puts "A"
+    end
+end
+
